be09607c8de4020e997019ccecf2049746008f08
sekod <sebastianalonsopinto@gmail.com>
2022-07-14 16:48:50 -0400

    CAMBIOS CRUD

--- crud/admin.py
+++ crud/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 admin.site.register(Marca)
 admin.site.register(Producto)
+admin.site.register(Tipo)
 
 
 # Register your models here.
--- crud/models.py
+++ crud/models.py
@@ -15,12 +15,27 @@ class Marca(models.Model):
     def __str__(self):
         return self.marca
 
+class Tipo(models.Model):
+    idTipo = models.IntegerField(primary_key=True, verbose_name='ID')
+    tipo = models.CharField(max_length=50, verbose_name='Tipo')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name='tipo'
+        verbose_name_plural='tipos'
+        ordering=['tipo']
+
+    def __str__(self):
+        return self.tipo
+
 class Producto(models.Model):
     idProducto = models.CharField(primary_key=True, max_length=10, verbose_name='ID')
     descripcion = models.CharField(max_length=100, verbose_name='Descripcion')
     precio = models.IntegerField(verbose_name='Precio Unitario')
     stock = models.IntegerField(verbose_name='Stock')
     marca = models.ForeignKey(Marca,on_delete=models.CASCADE,null=True,blank=True)
+    tipo = models.ForeignKey(Tipo,on_delete=models.CASCADE,null=True,blank=True)
     imagen = models.ImageField(verbose_name='Imagen',upload_to='productos',null=True,blank=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -32,5 +47,6 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.descripcion
+
 
 # Create your models here.
